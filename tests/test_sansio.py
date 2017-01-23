254759298f8eb5d02b351e2dbbd0a1ccf334696f
Brett Cannon <brett@python.org>
2017-01-22 17:34:52 -0800

    Keep ValidationFailure simple

--- tests/test_sansio.py
+++ tests/test_sansio.py
@@ -2,18 +2,6 @@ import pytest
 
 
 @pytest.mark.skip("not implemented")
-class TestValidationFailure:
-
-    """Tests for gidgethub.sansio.ValidationFailure."""
-
-    def test_simple_str(self):
-        """Test a message that has no formatting."""
-
-    def test_str_formatting(self):
-        """Test lazy string formatting."""
-
-
-@pytest.mark.skip("not implemented")
 class TestValidate:
 
     """Tests for gidgethub.sansio.validate()."""
