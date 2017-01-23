da92527ef6a79717a5ea1545a2b709dc41c9638c
Brett Cannon <brett@python.org>
2017-01-22 18:49:44 -0800

    Implement validate()

--- gidgethub/sansio.py
+++ gidgethub/sansio.py
@@ -16,6 +16,19 @@ class ValidationFailure(GitHubException):
 def validate(payload: bytes, *, signature: str, secret: str) -> None:
     """Validate that the webhook event body came from an approved repository.
 
-    ValidationFailure is raised if the payload cannot be validated.
+    ValidationFailure is raised if the provided signature does not match the
+    calculated signature.
     """
     # https://developer.github.com/webhooks/securing/#validating-payloads-from-github
+    signature_prefix = "sha1="
+    if not signature.startswith(signature_prefix):
+        raise ValidationFailure("signature does not start with "
+                                f"{repr(signature_prefix)}")
+    hash_ = hashlib.sha1()
+    hash_.update(secret.encode("UTF-8"))
+    hash_.update(payload)
+    calculated_sig = signature_prefix + hash_.hexdigest()
+    if not hmac.compare_digest(signature, calculated_sig):
+        raise ValidationFailure("payload's signature does not align with the "
+                                "secret")
+
--- tests/test_sansio.py
+++ tests/test_sansio.py
@@ -1,13 +1,24 @@
 import pytest
 
+from gidgethub import sansio
 
-@pytest.mark.skip("not implemented")
+
 class TestValidate:
 
     """Tests for gidgethub.sansio.validate()."""
 
+    secret = "123456"
+    payload = "gidget".encode("UTF-8")
+    hash_signature = "6ea124f8bfc2e6f5a0a40687201c351716110bec"
+    signature = "sha1=" + hash_signature
+
     def test_malformed_signature(self):
         """Error out if the signature doesn't start with "sha1="."""
+        with pytest.raises(sansio.ValidationFailure):
+            sansio.validate(self.payload, secret=self.secret,
+                            signature=self.hash_signature)
 
     def test_validation(self):
         """Success case."""
+        sansio.validate(self.payload, secret=self.secret,
+                        signature=self.signature)
