c5ba8b018b25aae09dc3c7ce57ad4f7e37807142
Brett Cannon <brett@python.org>
2017-02-11 17:13:18 -0800

    Script to convert curl header output to a JSON file

---
+++ gidgethub/test/samples/convert_headers.py
@@ -0,0 +1,20 @@
+"""Convert the header output from ``curl --head`` to a JSON object representing
+those headers.
+
+All keys will be lowercase and the status code will be used for the file name.
+"""
+import json
+import sys
+
+
+raw_headers = sys.stdin.read()
+print(raw_headers)
+header_lines = raw_headers.splitlines()
+status_code = int(header_lines[0].split()[1])
+del header_lines[0]
+headers = {}
+for line in header_lines:
+    key, _, value = line.partition(":")
+    headers[key.lower().strip()] = value.strip()
+with open(f"{status_code}.json", 'w') as file:
+    json.dump(headers, file, ensure_ascii=False)
