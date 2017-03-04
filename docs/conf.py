3eb8732c541594fe1f7943d81cab9707c94f011b
Brett Cannon <brett@python.org>
2017-03-03 16:17:07 -0800

    Add some custom directives for the docs

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- General configuration ------------------------------------------------
@@ -34,7 +34,9 @@
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.viewcode']
+    'sphinx.ext.viewcode',
+    'customization',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
---
+++ docs/customization.py
@@ -0,0 +1,33 @@
+from sphinx import addnodes
+from sphinx.domains.python import PyClassmember
+
+
+class PyCoroutine(PyClassmember):
+
+    def handle_signature(self, sig, signode):
+        ret = super().handle_signature(sig, signode)
+        signode.insert(0, addnodes.desc_annotation('coroutine ',
+                                                   'coroutine '))
+        return ret
+
+    def run(self):
+        self.name = 'py:method'
+        return PyClassmember.run(self)
+
+
+class PyAbstractCoroutine(PyCoroutine):
+
+    def handle_signature(self, sig, signode):
+        ret = super().handle_signature(sig, signode)
+        signode.insert(0, addnodes.desc_annotation('abstractmethod ',
+                                                   'abstractmethod '))
+        return ret
+
+    def run(self):
+        self.name = 'py:method'
+        return PyClassmember.run(self)
+
+
+def setup(app):
+    app.add_directive_to_domain('py', 'coroutine', PyCoroutine)
+    app.add_directive_to_domain('py', 'abstractcoroutine', PyAbstractCoroutine)
