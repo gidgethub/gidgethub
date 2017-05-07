e030a847e9b889dda2d701e455ad68ad09138fdb
Brett Cannon <brett@python.org>
2017-05-06 21:00:26 -0700

    Type hints for abc

--- gidgethub/abc.py
+++ gidgethub/abc.py
@@ -1,6 +1,7 @@
 """Provide an abstract base class for easier requests."""
 import abc
 import json
+from typing import Any, AsyncGenerator, Dict, Tuple, Optional
 
 from . import sansio
 
@@ -9,20 +10,22 @@ class GitHubAPI(abc.ABC):
 
     """Provide an idiomatic API for making calls to GitHub's API."""
 
-    def __init__(self, requester, *, oauth_token=None):
+    def __init__(self, requester: str, *, oauth_token: str = None) -> None:
         self.requester = requester
         self.oauth_token = oauth_token
         self.rate_limit: sansio.RateLimit = None
 
     @abc.abstractmethod
-    async def _request(self, method, url, headers, body=b''):
+    async def _request(self, method: str, url: str, headers: Dict,
+                       body: bytes = b'') -> Tuple[int, Dict, bytes]:
         """Make an HTTP request."""
 
     @abc.abstractmethod
-    async def sleep(self, seconds):
+    async def sleep(self, seconds: float) -> None:
         """Sleep for the specified number of seconds."""
 
-    async def _make_request(self, method, url, url_vars, data, accept):
+    async def _make_request(self, method: str, url: str, url_vars: Dict,
+                            data: Any, accept: str) -> Tuple[bytes, Optional[str]]:
         """Construct and make an HTTP request."""
         filled_url = sansio.format_url(url, url_vars)
         request_headers = sansio.create_headers(self.requester, accept=accept,
@@ -42,12 +45,14 @@ class GitHubAPI(abc.ABC):
         data, self.rate_limit, more = sansio.decipher_response(*response)
         return data, more
 
-    async def getitem(self, url, url_vars={}, *, accept=sansio.accept_format()):
+    async def getitem(self, url: str, url_vars: Dict = {},
+                      *, accept: str = sansio.accept_format()) -> Any:
         """Send a GET request for a single item to the specified endpoint."""
         data, _ = await self._make_request("GET", url, url_vars, b"", accept)
         return data
 
-    async def getiter(self, url, url_vars={}, *, accept=sansio.accept_format()):
+    async def getiter(self, url: str, url_vars: Dict = {},
+                      *, accept: str = sansio.accept_format()) -> AsyncGenerator[Any, None]:
         """Return an async iterable for all the items at a specified endpoint."""
         data, more = await self._make_request("GET", url, url_vars, b"", accept)
         for item in data:
@@ -57,17 +62,21 @@ class GitHubAPI(abc.ABC):
             async for item in self.getiter(more, url_vars, accept=accept):
                 yield item
 
-    async def post(self, url, url_vars={}, *, data, accept=sansio.accept_format()):
+    async def post(self, url: str, url_vars: Dict = {}, *, data: Any,
+                   accept: str = sansio.accept_format()) -> Any:
         data, _ = await self._make_request("POST", url, url_vars, data, accept)
         return data
 
-    async def patch(self, url, url_vars={}, *, data, accept=sansio.accept_format()):
+    async def patch(self, url: str, url_vars: Dict = {}, *, data: Any,
+                    accept: str = sansio.accept_format()) -> Any:
         data, _ = await self._make_request("PATCH", url, url_vars, data, accept)
         return data
 
-    async def put(self, url, url_vars={}, *, data=b"", accept=sansio.accept_format()):
+    async def put(self, url: str, url_vars: Dict = {}, *, data: Any = b"",
+                  accept: str = sansio.accept_format()) -> Any:
         data, _ = await self._make_request("PUT", url, url_vars, data, accept)
         return data
 
-    async def delete(self, url, url_vars={}, *, accept=sansio.accept_format()):
+    async def delete(self, url: str, url_vars: Dict = {}, *,
+                     accept: str = sansio.accept_format()) -> None:
         await self._make_request("DELETE", url, url_vars, b"", accept)
