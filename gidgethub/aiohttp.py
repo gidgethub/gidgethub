9fd8a04b3082ca72ed2536da4bd9ddf5be588e86
Brett Cannon <brett@python.org>
2017-06-02 15:42:13 -0700

    Make aiohttp.__init__() more flexible

--- gidgethub/aiohttp.py
+++ gidgethub/aiohttp.py
@@ -8,10 +8,9 @@ from . import abc as gh_abc
 
 class GitHubAPI(gh_abc.GitHubAPI):
 
-    def __init__(self, session: aiohttp.ClientSession, requester: str,
-                 *, oauth_token: str = None) -> None:
+    def __init__(self, session: aiohttp.ClientSession, *args, **kwargs) -> None:
         self._session = session
-        super().__init__(requester, oauth_token=oauth_token)
+        super().__init__(*args, **kwargs)
 
     async def _request(self, method: str, url: str, headers: Mapping,
                        body: bytes = b'') -> Tuple[int, Mapping, bytes]:
