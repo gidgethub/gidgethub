2d8c7daa51062679b8106fb1c51996c076c04b34
Brett Cannon <brett@python.org>
2017-06-04 12:19:47 -0700

    Make mypy happy

--- gidgethub/abc.py
+++ gidgethub/abc.py
@@ -69,7 +69,8 @@ class GitHubAPI(abc.ABC):
             if cacheable and has_cache_details:
                 etag = response[1].get("etag")
                 last_modified = response[1].get("last-modified")
-                self._cache[filled_url] = etag, last_modified, data, more
+                if self._cache is not None:
+                    self._cache[filled_url] = etag, last_modified, data, more
         return data, more
 
     async def getitem(self, url: str, url_vars: Dict = {},
