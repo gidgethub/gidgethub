762bacb16ab47a7402265eca44e334241e391c50
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2019-10-11 19:16:45 +0100

    Workaround Tornado's HTTPRequest arg typing error

--- gidgethub/tornado.py
+++ gidgethub/tornado.py
@@ -1,4 +1,4 @@
-from typing import Mapping, Optional, Tuple
+from typing import Mapping, Tuple
 
 from tornado import gen
 from tornado import httpclient
@@ -11,11 +11,14 @@ class GitHubAPI(gh_abc.GitHubAPI):
     async def _request(self, method: str, url: str, headers: Mapping[str, str],
                        body: bytes = b'') -> Tuple[int, Mapping[str, str], bytes]:
         """Make an HTTP request."""
-        if method == "GET" and not body:
-            real_body = None
-        else:
-            real_body = body
-        request = httpclient.HTTPRequest(url, method, headers, real_body)
+        # passing body = None raises an error in mypy:
+        # Argument 4 to "HTTPRequest" has incompatible type "Optional[bytes]";
+        # expected "Union[bytes, str]"
+        request = (
+            httpclient.HTTPRequest(url, method, dict(headers))
+            if method == "GET" and not body else
+            httpclient.HTTPRequest(url, method, dict(headers), body)
+        )
         # Since Tornado has designed AsyncHTTPClient to be a singleton, there's
         # no reason not to simply instantiate it every time.
         client = httpclient.AsyncHTTPClient()
