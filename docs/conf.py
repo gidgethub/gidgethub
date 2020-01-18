a9b4e37d295f6f814b3d6f165222dbcc3d8e84a9
Brett Cannon <54418+brettcannon@users.noreply.github.com>
2020-01-17 16:30:29 -0800

    Drop Travis and codecov buttons in the docs

--- docs/conf.py
+++ docs/conf.py
@@ -102,8 +102,6 @@ html_theme_options = {
     "github_repo": "gidgethub",
     "github_type": "star",
     "github_banner": True,
-    "travis_button": True,
-    "codecov_button": True,
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
