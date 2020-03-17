6ffc7394772143d0ddd2c2633342b0f9c1073266
Brett Cannon <brett@python.org>
2020-03-16 20:59:46 -0700

    Move to nox to run linting and testing

---
+++ noxfile.py
@@ -0,0 +1,32 @@
+import nox
+
+
+def install_flit_dev_deps(session):
+    session.install("flit")
+    session.run("flit", "install", "--deps", "develop")
+
+
+@nox.session(python=["3.6", "3.7", "3.8"])
+def tests(session):
+    install_flit_dev_deps(session)
+    session.run("pytest", "--cov", "tests")
+
+
+@nox.session
+def lint(session):
+    install_flit_dev_deps(session)
+    session.run("black", "--check", ".")
+    session.run("mypy", "--ignore-missing-imports", "--strict", "gidgethub")
+    session.run(
+        "sphinx-build",
+        "-nW",
+        "-q",
+        "-b",
+        "html",
+        "-b",
+        "linkcheck",
+        "-d",
+        "docs/_build/doctrees",
+        "docs",
+        "docs/_build/html",
+    )
