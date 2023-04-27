03a313125a34accb865a66e5351c4f0206e42754
Chris Rose <offline@offby1.net>
2023-04-27 08:34:10 -0600

    Update the intersphinx mapping to 1.0-style

--- docs/conf.py
+++ docs/conf.py
@@ -172,7 +172,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {"https://docs.python.org/3/": None}
+intersphinx_mapping = {"python": ("https://docs.python.org/3/", None)}
 
 
 html_sidebars = {
