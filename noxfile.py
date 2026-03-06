18d34c351fe38ab71fff246b723dafea772f5282
Brett Cannon <brett@python.org>
2026-03-06 14:43:11 -0800

    Move to dependency groups Along the way, modernize `pyproject.toml` and move to pdm-backend.

--- noxfile.py
+++ noxfile.py
@@ -1,20 +1,20 @@
 import nox
 
-
-def install_flit_dev_deps(session):
-    session.install("flit")
-    session.run("flit", "install", "--deps", "develop")
+PYPROJECT = nox.project.load_toml("pyproject.toml")
 
 
 @nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12", "3.13", "3.14", "3.15"])
 def tests(session):
-    install_flit_dev_deps(session)
+    session.install(
+        ".[aiohttp,tornado,httpx]",
+        *nox.project.dependency_groups(PYPROJECT, "test"),
+    )
     session.run("pytest", "--cov=gidgethub", "--cov-report=xml", "-n=auto", "tests")
 
 
 @nox.session
 def lint(session):
-    install_flit_dev_deps(session)
+    session.install(".", *nox.project.dependency_groups(PYPROJECT, "lint", "doc"))
     session.run("black", "--check", ".")
     session.run("mypy", "--ignore-missing-imports", "--strict", "gidgethub")
     session.run(
